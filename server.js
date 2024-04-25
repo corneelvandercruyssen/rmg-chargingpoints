31140c9b67018bf46831b4c090724a3ad881f813
corneelvandercruyssen <corneel@vandercruyssen.org>
2024-04-25 23:32:37 +0200

    Added Evere and SDW

--- server.js
+++ server.js
@@ -9,25 +9,71 @@ const PORT = process.env.PORT || 3000;  //De app is bereikbaar op x.x.x.x:3000
 
 app.get('/', async (req, res) => {
     try {
+
+        //RSL
         // Haal info af van Mobility Plus, betrouwbare source voor de laadpalen bij Roularta
-        const response = await fetch('https://my.mobilityplus.be/sp/api/v25/user/charging/evses/?charging_location=42017');
-        const data = await response.json();
+        const roeselareResponse = await fetch('https://my.mobilityplus.be/sp/api/v25/user/charging/evses/?charging_location=42017');
+        const roeselareData = await roeselareResponse.json();
 
-        // Tel uit de JSON uit die API call alle vrije laadplaen bij de Meiboomlaan
-        const availableCount = data.results.filter(evse => evse.status === 'available').length;
+        // Tel uit de JSON uit die API call alle vrije laadplaatsen bij Roularta
+        const roeselareAvailableCount = roeselareData.results.filter(evse => evse.status === 'available').length;
 
-        // Bereken hoe groot de groene balk moet zijn
-        const progressBarWidth = (availableCount / 20) * 100;
+        // Bereken hoe groot de groene balk moet zijn voor Roeselare
+        const roeselareProgressBarWidth = (roeselareAvailableCount / 20) * 100;
 
         // Minder dan 3 is rood, minder dan 5 is oranje , anders groen
-        let progressBarColor;
-        if (availableCount <= 3) {
-            progressBarColor = 'bg-danger';
-        } else if (availableCount <= 5) {
-            progressBarColor = 'bg-warning';
+        let roeselareProgressBarColor;
+        if (roeselareAvailableCount <= 3) {
+            roeselareProgressBarColor = 'bg-danger';
+        } else if (roeselareAvailableCount <= 5) {
+            roeselareProgressBarColor = 'bg-warning';
         } else {
-            progressBarColor = 'bg-success';
+            roeselareProgressBarColor = 'bg-success';
         }
+
+        //SDW
+        // Haal info af van SDW charging location
+        const sdwResponse = await fetch('https://my.mobilityplus.be/sp/api/v25/user/charging/evses/?charging_location=315665');
+        const sdwData = await sdwResponse.json();
+
+        // Tel uit de JSON uit die API call alle vrije laadplaatsen bij SDW
+        const sdwAvailableCount = sdwData.results.filter(evse => evse.status === 'available').length;
+
+        // Bereken hoe groot de groene balk moet zijn voor SDW
+        const sdwProgressBarWidth = (sdwAvailableCount / 4) * 100;
+
+        // Minder dan 3 is rood, minder dan 5 is oranje , anders groen
+        let sdwProgressBarColor;
+        if (sdwAvailableCount <= 1) {
+            sdwProgressBarColor = 'bg-danger';
+        } else if (sdwAvailableCount <= 2) {
+            sdwProgressBarColor = 'bg-warning';
+        } else {
+            sdwProgressBarColor = 'bg-success';
+        }
+
+        //BMC
+        // Haal info af van bmc charging location
+        const bmcResponse = await fetch('https://my.mobilityplus.be/sp/api/v25/user/charging/evses/?charging_location=42016');
+        const bmcData = await bmcResponse.json();
+
+        // Tel uit de JSON uit die API call alle vrije laadplaatsen bij bmc
+        const bmcAvailableCount = bmcData.results.filter(evse => evse.status === 'available').length;
+
+        // Bereken hoe groot de groene balk moet zijn voor bmc
+        const bmcProgressBarWidth = (bmcAvailableCount / 18) * 100;
+
+        // Minder dan 3 is rood, minder dan 5 is oranje , anders groen
+        let bmcProgressBarColor;
+        if (bmcAvailableCount <= 3) {
+            bmcProgressBarColor = 'bg-danger';
+        } else if (bmcAvailableCount <= 5) {
+            bmcProgressBarColor = 'bg-warning';
+        } else {
+            bmcProgressBarColor = 'bg-success';
+        }
+
+
 
         // Beantwoord met frontend html respons
         res.send(`
@@ -42,16 +88,29 @@ app.get('/', async (req, res) => {
             <body>
                 <div class="container">
                     <h1>Roularta vrije laadpunten</h1>
-					<h2>Roeselare<h2>
+                    <h2>Roeselare</h2>
                     <div class="progress" style="height: 30px;">
-                        <div id="progressBar" class="progress-bar ${progressBarColor}" role="progressbar" style="width: ${progressBarWidth}%; font-size: 20px;" aria-valuenow="${availableCount}" aria-valuemin="0" aria-valuemax="20"></div>
+                        <div id="roeselareProgressBar" class="progress-bar ${roeselareProgressBarColor}" role="progressbar" style="width: ${roeselareProgressBarWidth}%; font-size: 20px;" aria-valuenow="${roeselareAvailableCount}" aria-valuemin="0" aria-valuemax="20">${roeselareAvailableCount}</div>
                     </div>
-                    <div id="sensorValue" class="mt-3">Available Charging Points: ${availableCount}</div>
-					<h2>Evere - BMC<h2>
-					<h2>Sint-Denijs Western<h2>
+                    <div id="roeselareSensorValue" class="mt-3">Available Charging Points: ${roeselareAvailableCount}</div>
+
+                    <h2>Sint-Denijs-Westrem</h2>
+                    <div class="progress" style="height: 30px;">
+                        <div id="sdwProgressBar" class="progress-bar ${sdwProgressBarColor}" role="progressbar" style="width: ${sdwProgressBarWidth}%; font-size: 20px;" aria-valuenow="${sdwAvailableCount}" aria-valuemin="0" aria-valuemax="20">${sdwAvailableCount}</div>
+                    </div>
+                    <div id="sdwSensorValue" class="mt-3">Available Charging Points: ${sdwAvailableCount}</div>
+
+
+                    <h2>BMC - Evere</h2>
+                    <div class="progress" style="height: 30px;">
+                        <div id="bmcProgressBar" class="progress-bar ${bmcProgressBarColor}" role="progressbar" style="width: ${bmcProgressBarWidth}%; font-size: 20px;" aria-valuenow="${bmcAvailableCount}" aria-valuemin="0" aria-valuemax="20">${bmcAvailableCount}</div>
+                    </div>
+                    <div id="bmcSensorValue" class="mt-3">Available Charging Points: ${bmcAvailableCount}</div>
+
+
                 </div>
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/pop`per.js/1.16.0/umd/popper.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
             </body>
             </html>
@@ -62,7 +121,7 @@ app.get('/', async (req, res) => {
     }
 });
 
+// Start de server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
